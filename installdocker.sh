9e6104209fde45a40c7a9314d914cee94ed4b822
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2021-10-26 13:25:54 +0530

    Update installdocker.sh

--- installdocker.sh
+++ installdocker.sh
@@ -4,4 +4,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install -y docker-ce
-
+sudo usermod -aG docker ubuntu
