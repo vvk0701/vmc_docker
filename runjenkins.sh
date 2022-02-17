313046dd9e68299f8b9e8cb8cfedd82c3ffd8ee8
vvk0701 <56976878+vvk0701@users.noreply.github.com>
2022-02-17 18:23:09 +0530

    Update runjenkins.sh

--- runjenkins.sh
+++ runjenkins.sh
@@ -1,2 +1,2 @@
 docker build -t ujenkins .
-docker container run -d -p 8080:8080 -v /home/ubuntu/jenkins_vol:/var/jenkins_home --name jenkins_server  ujenkins
+docker container run -d -p 8080:8080 -v /root/jenkins_vol:/var/jenkins_home --name jenkins_server  ujenkins
